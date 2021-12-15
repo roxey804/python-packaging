a646a83293975f23883c690a17ba5e3704bd7a0d
Roxey <32243415+roxey804@users.noreply.github.com>
2021-12-15 21:51:42 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+   name='days_to_bday',
+   version='0.1.0',
+   author='Roxy',
+   author_email='roxy@example.com',
+   packages=['days_to_bday', 'days_to_bday.test'],
+   scripts=['bin/script1','bin/script2'],
+   url='http://github.com/pelucid/adr_client',
+   description='A package to fugre out how many days to your next birthday',
+   long_description=open('README.txt').read(),
+   install_requires=[
+       'pytest',
+       'pendulum',
+   ],
+)
