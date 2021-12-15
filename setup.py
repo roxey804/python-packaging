8e0c82fa5ba75950504141f7711a02fe663a372c
Roxey <32243415+roxey804@users.noreply.github.com>
2021-12-15 21:58:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
    author='Roxy',
    author_email='roxy@example.com',
    packages=['days_to_bday', 'days_to_bday.test'],
-   scripts=['bin/script1','bin/script2'],
+   scripts=['bin/script1','bin/script2'], #Not sure what these are for?
    url='http://github.com/pelucid/adr_client',
    description='A package to fugre out how many days to your next birthday',
    long_description=open('README.txt').read(),
